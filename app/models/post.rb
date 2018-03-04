f12b07a32cddc34f21f6ca1259895663a418356f
jiaowagn <wangjiaohere@gmail.com>
2018-03-04 14:47:30 +0800

    use scope to order by created_at to group's posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  validates_presence_of :content 
+  validates_presence_of :content
+
+  scope :recent, -> { order("created_at DESC") }
 end
